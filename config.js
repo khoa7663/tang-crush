1c917c0c5cdefb722d72bde15e9271a512414d54
khoa7663 <85656137+khoa7663@users.noreply.github.com>
2021-06-10 08:48:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Uki anh iu <33',
-    btnNo: 'Không đồng ý' :3',
+    btnNo: 'Không đồng ý :(' :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hết dịch anh qua đón em đi chơi nha công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Ukiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
