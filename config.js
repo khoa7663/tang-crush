b8ecd8f60059747297c51f9ff38b5aefe6f9d8a6
khoa7663 <85656137+khoa7663@users.noreply.github.com>
2021-06-10 08:42:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hết dịch anh qua đón em đi chơi nha công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Ukiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
